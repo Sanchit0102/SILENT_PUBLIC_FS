e71743b9e0d2a884fa21f77115f36dd4f39f2abe
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-04-03 14:04:21 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -13,7 +13,7 @@ class Config(object):
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "https://t.me/The_Silent_Teams")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001828164247"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
-  FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
+  FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", False))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
   BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
   OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
