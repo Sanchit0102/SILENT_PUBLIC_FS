587b1438b892b0a86e983702ff0d6b73795b4009
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-04-03 13:13:12 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -62,7 +62,7 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
             disable_web_page_preview=True
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Share Link", url=share_link)]])
             )
-        )
+        
         await bot.send_message(
             chat_id=int(Config.LOG_CHANNEL),
             text=f"#BATCH_SAVE:\n\n[{editable.reply_to_message.from_user.first_name}](tg://user?id={editable.reply_to_message.from_user.id}) Got Batch Link!",
