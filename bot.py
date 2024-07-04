742cc541d7d1a782c7a852ea51263beff2912c88
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-07-04 19:02:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -356,13 +356,13 @@ async def button(bot: Client, cmd: CallbackQuery):
                     )
                     return
             except UserNotParticipant:
-                invite_link = await get_invite_link(channel_chat_id)
+                # invite_link = await get_invite_link(channel_chat_id)
                 await cmd.message.edit(
                     text="**I like Your Smartness But Don't Be Oversmart! 😑**\n\n",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)
+                                InlineKeyboardButton("🤖 Join Updates Channel", url="https://t.me/BOT_TESTING_OFFICIAL")
                             ],
                             [
                                 InlineKeyboardButton("🔄 Refresh 🔄", callback_data="refreshmeh")
