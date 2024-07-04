d252d96ca577818af5b91d64d9678afd9442e9c9
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-07-04 16:23:18 +0530

    Force Sub On 🔥

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -32,7 +32,7 @@ async def handle_force_sub(bot: Client, cmd: Message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [𝐎𝐰𝐧𝐞𝐫](https://t.me/THE_DS_OFFICIAL).",
+                text="Sorry, You are Banned to use me. Contact my [𝐎𝐰𝐧𝐞𝐫](https://t.me/THE_DS_OFFICIAL).",
                 disable_web_page_preview=True
             )
             return 400
